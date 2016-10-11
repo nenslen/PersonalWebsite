574f5f80aa18bf0c5354da2f575371b71465335f
nenslen <nicolas.enslen@gmail.com>
2016-10-10 21:29:25 -0600

    First Commit
    First Commit

---
+++ index.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title>Nicolas Enslen</title>
+	<meta charset='utf-8'>
+	<meta name='viewport' content='width=device-width; initial-scale=1'>
+	<link rel='stylesheet' href='styles/style-home.css'>
+	<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css'>
+</head>
+<body>
+	<div id='home-head'>
+		<div id='titles'>
+			<h1 id='full-name'>Nicolas Enslen</h1>
+			<h4 id='titles'>Programmer, Web Developer</h4>
+		</div>
+	</div>
+	<!--
+	<div id='top-right'>
+		<a href='mailto:nicolas.enslen@gmail.com'><i class='fa fa-envelope fa-3x' aria-hidden='true'></i></a>
+		<a href='https://github.com/nenslen' target='_blank'><i class='fa fa-github fa-3x' aria-hidden='true'></i></a>
+		<a href='https://ca.linkedin.com/in/nicolas-enslen-8a3b7b119' target='_blank'><i class='fa fa-linkedin fa-3x' aria-hidden='true'></i></a>
+		</br></br>
+	</div>
+	-->
+	
+
+	<div class='wrapper'>
+		
+		<!-- About Section -->
+		<div class='main-content' id='about'>
+			<h1>About</h1>
+			<div class='about-content'>
+				<img src='images/self-image.jpg' height='200px' width='200' />
+			</div>
+			<div class='about-content'>
+				<p>I love programming! Making cool websites, applications, and games is fun, and it's what I like to do.</p>
+				<p>I'm currently in my third year of computer science at the University of Lethbridge.</p>
+			</div>
+		</div>
+		
+		
+
+		<!-- Projects Section -->
+		<div class='main-content' id='project-description'>
+			<h1>Projects</h1>
+			<p>
+				This is a sample of some of my favorite projects that I've made.
+				You can see a larger list of my projects on <a href='https://github.com/nenslen' target='_blank'>GitHub</a>.
+			</p>
+		</div>
+		
+		<div id='projects'>
+			<div class="cards">
+
+				<!-- Block Puzzle -->
+				<div class="card">
+					<div class="card-image">
+						<div class='video'>
+							<div class="video-wrapper">
+								<iframe width="420" height="315" src="https://www.youtube.com/embed/EL6INBXauik?autoplay=0" frameborder="0" allowfullscreen></iframe>
+							</div>
+						</div>
+					</div>
+					<div class="card-header">
+						Block Puzzle (Java)
+					</div>
+					<div class="card-copy">
+						<p>In this game, you place blocks onto the grid. When a column or row is full, those
+						blocks disappear. This game is a recreation of a game on the Google Playstore called
+						'Block Puzzle'.</p>
+						<p><a href='https://github.com/nenslen/BlockPuzzle' target='_blank'>Project on GitHub</a></p>
+						<p><a href='https://github.com/nenslen/BlockPuzzle/releases/download/1.0/Block_Puzzle.jar'>Download .jar</a></p>
+					</div>
+				</div>
+
+
+				<!-- Snake -->
+				<div class="card">
+					<div class="card-image">
+						<div class='video'>
+							<div class="video-wrapper">
+								<iframe width="420" height="315" src="https://www.youtube.com/embed/uENgQ7qtVaE?autoplay=0" frameborder="0" allowfullscreen></iframe>
+							</div>
+						</div>
+					</div>
+					<div class="card-header">
+						Snake (Java)
+					</div>
+					<div class="card-copy">
+						<p>This is a recreation of the classic game 'Snake' with 3 game modes.</p>
+						<p><a href='https://github.com/nenslen/Snake' target='_blank'>Project on GitHub</a></p>
+					</div>
+				</div>
+
+
+				<!-- Test -->
+				<div class="card">
+					<div class="card-image">
+						<div class='video'>
+							<div class="video-wrapper">
+								<img src="images/site.png"></img>
+							</div>
+						</div>
+					</div>
+					<div class="card-header">
+						This website (HTML, CSS/SCSS)
+					</div>
+					<div class="card-copy">
+						<p>I created this website. Nothing fancy, just HTML and CSS/SCSS were used.</p>
+						<p><a href='https://github.com/nenslen/PersonalWebsite' target='_blank'>Project on GitHub</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+
+		<!-- Knowledge Section -->
+		<div class='main-content' id='knowledge'>
+			<h1>Knowledge</h1>
+			
+			<div>
+				<div class='knowledge-square'><div class='knowledge-content'>HTML</div></div>
+				<div class='knowledge-square'><div class='knowledge-content'>CSS/SCSS</div></div>
+				<div class='knowledge-square'><div class='knowledge-content'>JavaScript</div></div>
+				<div class='knowledge-square'><div class='knowledge-content'>PHP</div></div>
+				<div class='knowledge-square'><div class='knowledge-content'>AngularJS</div></div>
+				<div class='knowledge-square'><div class='knowledge-content'>C++</div></div>
+				<div class='knowledge-square'><div class='knowledge-content'>C#</div></div>
+				<div class='knowledge-square'><div class='knowledge-content'>Java</div></div>
+				<div class='knowledge-square'><div class='knowledge-content'>SQL</div></div>
+			</div>
+		</div>
+		
+
+
+		<!-- Education Section -->
+		<div class='main-content' id='education'>
+			<h1>Education</h1>
+			
+			<div class='education-content'>
+				<!--<img src='images/college-icon.png' width='128px' height='128px' />-->
+				<h3 class='education-header'>Lethbridge College</h3>
+				<p>Diploma in Computer Information Technology (2016)</p>
+			</div>
+			<div class='education-content'>
+				<!--<img src='images/university-icon.png' width='128px' height='128px' />-->
+				<h3 class='education-header'>University of Lethbridge</h3>
+				<p>Bachelor of Computer Science (2018)</p>
+			</div>
+		</div>
+		
+		
+		
+		<!-- Contact Section -->
+		<div class='main-content' id='contact'>
+			<h1>Contact</h1>
+			
+			<div class='contact-content' id="contact-email">
+				<a href='mailto:nicolas.enslen@gmail.com'><i class='fa fa-envelope fa-5x' aria-hidden='true'></i></a>
+				<p><a href='mailto:nicolas.enslen@gmail.com'>nicolas.enslen@gmail.com</a></p>
+			</div>
+			
+			<div class='contact-content'>
+				<a href='https://github.com/nenslen' target='_blank'><i class='fa fa-github fa-5x' aria-hidden='true'></i></a>
+				<p><a href='https://github.com/nenslen' target='_blank'>github</a></p>
+			</div>
+			
+			<div class='contact-content'>
+				<a href='https://ca.linkedin.com/in/nicolas-enslen-8a3b7b119' target='_blank'><i class='fa fa-linkedin fa-5x' aria-hidden='true'></i></a>
+				<p><a href='https://ca.linkedin.com/in/nicolas-enslen-8a3b7b119' target='_blank'>linkedin</a></p>
+			</div>
+		</div>
+
+
+
+		<footer>
+			&copy 2016 - Nic Enslen
+		</footer>
+	</div> <!-- wrapper -->
+</body>
+</html>
