a9fa82e2cdf8239b804b98d5bf0d8758b21bd6a9
nenslen <nicolas.enslen@gmail.com>
2018-04-01 21:30:45 -0600

    Fixed header sizing issue when changing pages

--- js/home.js
+++ js/home.js
@@ -1,6 +1,6 @@
 window.addEventListener("beforeunload", function(event) {
 	console.log("scrolling");
-    window.scrollTo(0, 0);
+    //window.scrollTo(0, 0);
 });
 
 
@@ -9,7 +9,22 @@ var initialHeight = 0;
 $(function() {
 	//console.log("kdjf");
 	//$(this).scrollTop(0);
+	
 	initialHeight = $('#header').outerHeight();
+
+	var scrollDistance = $(document).scrollTop();
+	var wrapperMargin = parseInt($(".wrapper").css("marginTop"));
+	var target = wrapperMargin - 45;
+
+	//$('#header').height(initialHeight - scrollDistance);
+	if(scrollDistance > target){
+		$("#header").removeClass("header-large");
+		$("#header").addClass("header-small");
+	} else {
+		$("#header").removeClass("header-small");
+		$("#header").addClass("header-large");
+	}
+
 });
 
 $(document).on("scroll", function(){
