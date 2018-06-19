f0406b045c2e0287a91fede014ab2e4a30cd1ab6
nenslen <nicolas.enslen@gmail.com>
2018-06-18 18:09:12 -0700

    Updated descriptions of projects

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <!DOCTYPE html>
 <?php require 'functions.php'; ?>
+<?php
+	// Check for apple devices
+	$userAgent = $_SERVER['HTTP_USER_AGENT'];
+	$appleDevice = false;
+	if(stripos($userAgent, 'Chrome') == false && stripos($userAgent, 'Safari') == true) {
+		$appleDevice = true;
+    }
+?>
 <html>
 <head>
 	<?php drawHead('Nicolas Enslen - Programmer, Web Developer', ''); ?>
@@ -45,24 +53,30 @@
 		
 					<div class="grid grid-3-2-1">
 
-						<!-- Neural Network -->
+						<!-- Digit Recognition -->
 						<div class="card-outer">
 							<div class="card">
+							<?php if($appleDevice) : ?>
+								<div class="card-media" id="neuralnetwork.png">
+									<img src="media/neuralnetwork.png"></img>
+								</div>
+							<?php else : ?>
 								<div class="card-media" id="neuralnetwork.webm">
 									<video controls autoplay loop muted>
 										<source src="media/neuralnetwork.webm" type="video/webm">
 									</video>
 								</div>
+							<?php endif; ?>
 
 								<div class="card-title">
-									Digit Recognition (JS)
+									Digit Recognition
 								</div>
 
 								<input type="checkbox" class="read-more-state" id="item-1" />
 								<label for="item-1" class="read-more-trigger"></label>
 								<div class="read-more-wrap card-description">
 									<div class="read-more-target">
-										<p class="text-align-left">A neural network made from scratch that can recognize handwritten digits. This is my most difficult, but most rewarding project that I have undertaken so far! I had a lot of fun getting introduced to the topic of artificial intelligence while working on it!</p>
+										<p class="text-align-left">A neural network made from scratch that can recognize handwritten digits. In its current state, it's about 80% accurate when it looks at numbers I draw, and was 90% accurate on the data it was trained with. This is far from perfect (the record is 99.79% accuracy), but I wanted to show it now because I learned so much and had a lot of fun while working on it! I am currently learning the necessary math to bring the accuracy up to ~99%, and will update once that is achieved.</p>
 										<!--<p><a href='' target='_blank'>Full Video on Youtube</a></p>-->
 										<p class="text-align-left"><a href='https://github.com/nenslen/digit-recognition' target='_blank'>Project on GitHub</a></p>
 										<p class="text-align-left"><a href='projects/demos/digit-recognition/'>Demo</a></p>
@@ -70,28 +84,34 @@
 								</div>
 							</div>
 						</div>
-						<!-- end Neural Network -->
+						<!-- end Digit Recognition -->
 
 
 
 						<!-- Block Puzzle -->
 						<div class="card-outer">
 							<div class="card">
-								<div class="card-media" id="blockpuzzle.webm">
-									<video controls autoplay loop muted>
-										<source src="media/blockpuzzle.webm" type="video/webm">
-									</video>
-								</div>
+								<?php if($appleDevice) : ?>
+									<div class="card-media" id="blockpuzzle.png">
+										<img src="media/blockpuzzle.png"></img>
+									</div>
+								<?php else : ?>
+									<div class="card-media" id="blockpuzzle.webm">
+										<video controls autoplay loop muted>
+											<source src="media/blockpuzzle.webm" type="video/webm">
+										</video>
+									</div>
+								<?php endif; ?>
 
 								<div class="card-title">
-									Block Puzzle (Java)
+									Block Puzzle
 								</div>
 
 								<input type="checkbox" class="read-more-state" id="item-2" />
 								<label for="item-2" class="read-more-trigger"></label>
 								<div class="read-more-wrap card-description">
 									<div class="read-more-target">
-										<p class="text-align-left">This game is a re-creation of a game on the Google Playstore called 'Block Puzzle'. You place blocks onto the grid, and when a column or row is full, those blocks disappear. While making this game I learned more about how java handles dynamic layouts and events. I'm also currently making an online multiplayer version of this game that uses a NodeJS server to host the lobbies.</p>
+										<p class="text-align-left">This game is a re-creation of a game on the Google Playstore called 'Block Puzzle'. You place blocks onto the grid, and when a column or row is full, those blocks disappear. While making this game I learned more about how java handles dynamic layouts and events. I'm also currently making an online multiplayer version of this game that uses NodeJS to host the lobbies.</p>
 										<p class="text-align-left"><a href='https://www.youtube.com/watch?v=EL6INBXauik' target='_blank'>Full Video on Youtube</a></p>
 										<p class="text-align-left"><a href='https://github.com/nenslen/BlockPuzzle' target='_blank'>Project on GitHub</a></p>
 										<p class="text-align-left"><a href='https://github.com/nenslen/BlockPuzzle/releases/download/1.0/Block_Puzzle.jar'>Download .jar</a></p>
@@ -102,59 +122,38 @@
 						<!-- end Block Puzzle -->
 
 
+
 						<!-- Connect Four -->
 						<div class="card-outer">
 							<div class="card">
-								<div class="card-media" id="connect-four.webm">
-									<video controls autoplay loop muted>
-										<source src="media/connect-four.webm" type="video/webm">
-									</video>
-								</div>
+								<?php if($appleDevice) : ?>
+									<div class="card-media" id="connect-four.png">
+										<img src="media/connect-four.png"></img>
+									</div>
+								<?php else : ?>
+									<div class="card-media" id="connect-four.webm">
+										<video controls autoplay loop muted>
+											<source src="media/connect-four.webm" type="video/webm">
+										</video>
+									</div>
+								<?php endif; ?>
 
 								<div class="card-title">
-									Connect 4 AI (JS)
+									Connect Four AI
 								</div>
 
 								<input type="checkbox" class="read-more-state" id="item-9" />
 								<label for="item-9" class="read-more-trigger"></label>
 								<div class="read-more-wrap card-description">
 									<div class="read-more-target">
-										<p class="text-align-left">Connect Four, featuring an AI opponent that uses some neat methods to play. This project started as an assignment question for a university class and quickly became one of my favorite projects so far! While working on this project I gained a much deeper understanding of how to precisely define a problem and then solve it using smart search algorithms.
+										<p class="text-align-left">Connect Four, featuring an AI opponent that uses some neat methods to play. This project started as an assignment question for a university class and quickly became one of my favorite projects so far! While working on this project I gained a much deeper understanding of how to precisely define a problem and then solve it using smart search algorithms. 
 										<p class="text-align-left"><a href='https://github.com/nenslen/connect-four' target='_blank'>Project on GitHub</a></p>
 										<p class="text-align-left"><a href='projects/demos/connect-four/'>Demo</a></p>
 									</div>
 								</div>
 							</div>
 						</div>
-						<!-- end Neural Network -->
-
-
-
-						<!-- Snake -->
-						<div class="card-outer">
-							<div class="card">
-								<div class="card-media" id="snake.webm">
-									<video controls autoplay loop muted>
-										<source src="media/snake.webm" type="video/webm">
-									</video>
-								</div>
-
-								<div class="card-title">
-									Snake (Java)
-								</div>
-
-								<input type="checkbox" class="read-more-state" id="item-3" />
-								<label for="item-3" class="read-more-trigger"></label>
-								<div class="read-more-wrap card-description">
-									<div class="read-more-target">
-										<p class="text-align-left">This is a re-creation of the classic game Snake. While making this game, I became much better at dealing with grids and applying user-defined settings (speed, size, wrap, etc.) to the game. I've also started working on an AI that controls the snake using some interesting search methods, and will update the project once finished.</p>
-										<p class="text-align-left"><a href='https://www.youtube.com/watch?v=uENgQ7qtVaE' target='_blank'>Full Video on Youtube</a></p>
-										<p class="text-align-left"><a href='https://github.com/nenslen/Snake' target='_blank'>Project on GitHub</a></p>
-									</div>
-								</div>
-							</div>
-						</div>
-						<!-- end Snake -->
+						<!-- end Connect Four -->
 
 
 
@@ -166,75 +165,20 @@
 								</div>
 
 								<div class="card-title">
-									This Website (PHP, JS, SCSS)
+									nicolasenslen.com
 								</div>
 
 								<input type="checkbox" class="read-more-state" id="item-4" />
 								<label for="item-4" class="read-more-trigger"></label>
 								<div class="read-more-wrap card-description">
 									<div class="read-more-target">
-										<p class="text-align-left">This website has seen many improvements since I started working on it over 2 years ago. The thing I enjoyed most while creating this website was how to make everything responsive. It's really satisfying to see the elements adapt to different screen sizes!</p>
+										<p class="text-align-left">This website has seen many iterations and improvements since I started working on it over 2 years ago. Every time I learn a new tool or technique, I see if I can find a way to apply it to my website. This allows me to improve both my skills and website through hands-on experimentation.</p>
 										<p class="text-align-left"><a href='https://github.com/nenslen/PersonalWebsite' target='_blank'>Project on GitHub</a></p>
 									</div>
 								</div>
 							</div>
 						</div>
 						<!-- end Personal Website -->
-
-
-
-						<!-- Calculator -->
-						<div class="card-outer">
-							<div class="card">
-								<div class="card-media" id="calculator.png">
-									<img src="media/calculator.png"></img>
-								</div>
-
-								<div class="card-title">
-									Calculator (JS)
-								</div>
-
-								<input type="checkbox" class="read-more-state" id="item-5" />
-								<label for="item-5" class="read-more-trigger"></label>
-								<div class="read-more-wrap card-description">
-									<div class="read-more-target">
-										<p class="text-align-left">This is a calculator that I created for a project on freecodecamp.com. It's fairly basic as far as calculators go, but I found some of the operation chaining to be pretty cool!</p>
-										<p class="text-align-left"><a href='https://codepen.io/NicEnslen/pen/BQPvmW/' target='_blank'>Code on Codepen</a></p>
-										<p class="text-align-left"><a href='https://codepen.io/NicEnslen/full/BQPvmW/' target='_blank'>Demo</a></p>
-									</div>
-								</div>
-							</div>
-						</div>
-						<!-- end Calculator -->
-
-
-
-						<!-- Othello -->
-						<div class="card-outer">
-							<div class="card">
-								<div class="card-media" id="othello.webm">
-									<video controls autoplay loop muted>
-										<source src="media/othello.webm" type="video/webm">
-									</video>
-								</div>
-
-								<div class="card-title">
-									Othello (C++)
-								</div>
-
-								<input type="checkbox" class="read-more-state" id="item-6" />
-								<label for="item-6" class="read-more-trigger"></label>
-								<div class="read-more-wrap card-description">
-									<div class="read-more-target">
-										<p class="text-align-left">This is Othello. I made it for a project in one of my programming classes. In addtion to the full game, we had to include features
-										   that would allow the user to change the colors of the player pieces and board.</p>
-										<p class="text-align-left"><a href='https://www.youtube.com/watch?v=DcL2g_oJmbg' target='_blank'>Full Video on Youtube</a></p>
-										<p class="text-align-left"><a href='https://github.com/nenslen/Othello' target='_blank'>Project on GitHub</a></p>
-									</div>
-								</div>
-							</div>
-						</div>
-						<!-- end Othello -->
 
 
 
@@ -246,14 +190,14 @@
 								</div>
 
 								<div class="card-title">
-									Database (PHP, Angular, MySQL)
+									Parts Database
 								</div>
 
 								<input type="checkbox" class="read-more-state" id="item-7" />
 								<label for="item-7" class="read-more-trigger"></label>
 								<div class="read-more-wrap card-description">
 									<div class="read-more-target">
-										<p class="text-align-left">This was a project for a class in college. It feels a little dated now compared to other projects I've made since this one, but I learned a lot about PHP, Angular, and MySQL. The main goal was to use PHP to interface with MySQL, then use Angular to display the data.</p>
+										<p class="text-align-left">This college project accesses and displays information stored in a database. The interface feels dated considering all that I've learned since I created it, but the main focus of this project was to utilize technologies such as PHP, Angular, and MySQL.</p>
 										<p class="text-align-left"><a href='https://github.com/nenslen/computer-site' target='_blank'>Project on GitHub</a></p>
 										<p class="text-align-left"><a href='projects/demos/computer-site/'>Demo</a></p>
 									</div>
@@ -267,21 +211,27 @@
 						<!-- Ultimate Tic Tac Toe -->
 						<div class="card-outer">
 							<div class="card">
-								<div class="card-media" id="tictactoe.webm">
-									<video controls autoplay loop muted>
-										<source src="media/tictactoe.webm" type="video/webm">
-									</video>
-								</div>
+								<?php if($appleDevice) : ?>
+									<div class="card-media" id="tictactoe.png">
+										<img src="media/tictactoe.png"></img>
+									</div>
+								<?php else : ?>
+									<div class="card-media" id="tictactoe.webm">
+										<video controls autoplay loop muted>
+											<source src="media/tictactoe.webm" type="video/webm">
+										</video>
+									</div>
+								<?php endif; ?>
 
 								<div class="card-title">
-									Ultimate Tic Tac Toe (JS)
+									Ultimate Tic Tac Toe
 								</div>
 
 								<input type="checkbox" class="read-more-state" id="item-8" />
 								<label for="item-8" class="read-more-trigger"></label>
 								<div class="read-more-wrap card-description">
 									<div class="read-more-target">
-										<p class="text-align-left">It's like regular tic tac toe, but in order to win you need to win 3 small games in a row. While working on this project I learned more about jQuery and how to handle inputs from 81 different elements.</p>
+										<p class="text-align-left">It's like regular tic tac toe, but in order to win you need to win 3 small games in a row. While working on this project I specifically learned more about jQuery and how to handle inputs from a large amount of dynamic elements.</p>
 										<p class="text-align-left"><a href='https://youtu.be/6TgGcMFGhXo' target='_blank'>Full Video on Youtube</a></p>
 										<p class="text-align-left"><a href='https://github.com/nenslen/ultimate-tic-tac-toe' target='_blank'>Project on GitHub</a></p>
 										<p class="text-align-left"><a href='projects/demos/ultimate-tic-tac-toe/'>Demo</a></p>
@@ -296,7 +246,7 @@
 
 
 
-		<!-- Knowledge -->
+		<!-- Knowledge --
 		<div class='section-wrapper'>
 			<div class="section" id="knowledge">
 				<h1 class="section-header" id="knowledge-header">Knowledge</h1>
@@ -311,7 +261,7 @@
 					<div class='knowledge-square'><div class='knowledge-content'>Java</div></div>
 					<div class='knowledge-square'><div class='knowledge-content'>SQL</div></div>
 				</div>
-			</div><!-- End section -->
+			</div><!-- End section --
 		</div><!-- End Knowledge -->
 		
 
@@ -349,6 +299,7 @@
 								</div>
 							</div>
 						</div><!-- End University of Lethbridge -->
+
 					</div><!-- End grid -->
 				</div><!-- End section content -->
 			</div><!-- End section -->
